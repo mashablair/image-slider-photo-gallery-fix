9d9b5409888c5af255009f8f0603b30546c8e40a
Masha Blair <mashablair@gmail.com>
2018-08-03 17:56:28 -0700

    Add 2 versions of nav arrows, and fix for pagination/caption only to be visible on active image.

--- photo-gallery.js
+++ photo-gallery.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
+	'use strict';
 
     var photoGallery = new Swiper('#photoGallery .swiper-container', {
         slidesPerView: 'auto',
         centeredSlides: true,
         paginationClickable: true,
         spaceBetween: 10,
-        loop: true,
+		navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev'
+	    },
+//        loop: true,
         keyboardControl: true,
-        autoplay: 4000,
+//        autoplay: 4000,
         onSlideChangeEnd: function (swiper) {
             $('#photoGalleryCaption').html($('.swiper-slide.swiper-slide-active img').prop('alt')); // set first caption in the image viewer
             $('.swiper-slide.swiper-slide-active img').focus(); // set focus to the active slide's image
         }
     });
-
+	
 });
